858eee0f4a57dff2917d5d0d3fd8c3aa5d506c05
umayuslu <umayuslu@hotmail.com>
2025-03-14 14:47:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Araba
+{
+    public string Marka { get; set; }
+    public double Benzintuketimi{ get; set; }
+    
+    
+}
+
+class Program
+{static void Main()
+    {
+        List<Araba> arabalar = new List<Araba>() ;
+        Araba bmw = new Araba() { Marka = "BMW", Benzintuketimi = 30 };
+        Araba porsche = new Araba() { Marka = "PORSCHE", Benzintuketimi = 20 };
+        Araba mercedes = new Araba() { Marka = "MERCESHE", Benzintuketimi = 34 };
+        arabalar.Add(bmw);
+        arabalar.Add(porsche);
+        arabalar.Add(mercedes);
+
+        double totalyakittuketimi=0;
+        
+        foreach (var araba in arabalar)
+        {
+            Console.WriteLine("MARKA: " + araba.Marka + " YAKIT TÜKETİMİ: " + araba.Benzintuketimi);
+
+        }
+        
+        foreach (var araba in arabalar)
+        {
+            totalyakittuketimi+=araba.Benzintuketimi;
+            Console.WriteLine("Toplam yakıt tüketimi:" + totalyakittuketimi);
+
+        }
+
+    }
+}
